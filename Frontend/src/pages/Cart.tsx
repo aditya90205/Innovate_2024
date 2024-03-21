99f01da42b8627eeffc9c47ec0fae7256d5ea0cb
aditya90205 <adityasrivastav90205@gmail.com>
2024-03-21 22:23:54 +0530

    Addee Home and Cart section

--- Frontend/src/pages/Cart.tsx
+++ Frontend/src/pages/Cart.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
+
+const cartItems = [
+  {
+    productId: "asda",
+    photo: "https://m.media-amazon.com/images/I/71vFKBpKakL._SX522_.jpg",
+    name: "Macbook",
+    price: 12000,
+    quantity: 4,
+    stock: 10,
+  },
+];
+const subTotal = 4000;
+const tax = Math.round(subTotal * 0.18);
+const shippingCharges = 400;
+const discount = 400;
+const total = subTotal + tax + shippingCharges;
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
 
-export default Cart
+  useEffect(() => {
+    const timeOutID = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutID);
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  return (
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? cartItems.map((i, index) => (
+          <CartItem key={index} cartItem={i} />
+        )): (<h1>No Items Added</h1>)}
+      </main>
+      <aside>
+        <p>SubTotal: ₹{subTotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em className="green"> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+        <input
+          placeholder="Coupon Code"
+          type="text"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Code <VscError />
+            </span>
+          ))}
+          {
+            cartItems.length > 0 && <Link to="/shipping">Checkout</Link>
+          }
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const addToCartHandler = () => {};
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
 
-export default Home
+      <main>
+        <ProductCard
+          productId="asda"
+          name="Macbook"
+          price={12000}
+          stock={5}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/I/71vFKBpKakL._SX522_.jpg"
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
