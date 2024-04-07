713b6eea186f9b2eda871ce1aafb2067e7cd7185
aditya90205 <adityasrivastav90205@gmail.com>
2024-04-07 21:06:43 +0530

    Complete Users controller auth routes type

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,8 +3,12 @@ import express from "express";
 
 // Importing Routes
 import userRoute from "./routes/user.routes.js"
+import { connectDB } from "./utils/connectDB.js";
+import { errorMiddleware } from "./middlewares/error.js";
 const port = 4000;
+connectDB();
 const app = express();
+app.use(express.json());
 
 app.get('/', (req,res)=> {
     res.send('API working with /api/v1');
@@ -12,6 +16,10 @@ app.get('/', (req,res)=> {
 
 // Usind Routes
 app.use('/api/v1/user', userRoute);
+
+
+// Error Middleware
+app.use(errorMiddleware);
 
 app.listen(port, () => {
     console.log(`Server is working on ${port}`);
