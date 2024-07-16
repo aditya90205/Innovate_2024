2245d02c7bf1f032d0b5828bf373e97bd3234f90
aditya90205 <adityasrivastav90205@gmail.com>
2024-07-17 00:15:35 +0530

    Update userAPI.ts to include deleteUser mutation and allUsers query

--- Frontend/src/pages/admin/customers.tsx
+++ Frontend/src/pages/admin/customers.tsx
@@ -1,8 +1,16 @@
-import { ReactElement, useState } from "react";
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import { ReactElement, useEffect, useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { Column } from "react-table";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 import TableHOC from "../../components/admin/TableHOC";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useAllUsersQuery, useDeleteUserMutation } from "../../redux/api/userAPI";
+import toast from "react-hot-toast";
+import { CustomError } from "../../types/api-types";
+import { Skeleton } from "../../components/Loader";
+import { responseToast } from "../../utils/features";
 
 interface DataType {
   avatar: ReactElement;
@@ -40,55 +48,36 @@ const columns: Column<DataType>[] = [
   },
 ];
 
-const img = "https://randomuser.me/api/portraits/women/54.jpg";
-const img2 = "https://randomuser.me/api/portraits/women/50.jpg";
-
-const arr: Array<DataType> = [
-  {
-    avatar: (
-      <img
-        style={{
-          borderRadius: "50%",
-        }}
-        src={img}
-        alt="Shoes"
-      />
-    ),
-    name: "Emily Palmer",
-    email: "emily.palmer@example.com",
-    gender: "female",
-    role: "user",
-    action: (
-      <button>
-        <FaTrash />
-      </button>
-    ),
-  },
-
-  {
-    avatar: (
-      <img
-        style={{
-          borderRadius: "50%",
-        }}
-        src={img2}
-        alt="Shoes"
-      />
-    ),
-    name: "May Scoot",
-    email: "aunt.may@example.com",
-    gender: "female",
-    role: "user",
-    action: (
-      <button>
-        <FaTrash />
-      </button>
-    ),
-  },
-];
-
 const Customers = () => {
-  const [rows, setRows] = useState<DataType[]>(arr);
+  const { user } = useSelector((state: RootState) => state.userReducer);
+
+  const { isLoading, data, isError, error } = useAllUsersQuery(user?._id!);
+
+  const [rows, setRows] = useState<DataType[]>([]);
+
+  const [deleteUser] = useDeleteUserMutation();
+
+  const deleteHandler = async (userId:string) => {
+     const res = await deleteUser({userId, adminUserId: user?._id!});
+     responseToast(res, null, "");
+  }
+
+  if (isError) toast.error((error as CustomError).data.message);
+
+  useEffect(() => {
+    if (data) {
+      setRows(
+        data.users.map((i) => ({
+          avatar: <img style={{borderRadius: "50%"}} src={i.photo} alt={i.name} />,
+          name: i.name,
+          email: i.email,
+          gender: i.gender,
+          role: i.role,
+          action: <button onClick={() => deleteHandler(i._id)}><FaTrash /></button>,
+        }))
+      );
+    }
+  }, [data]);
 
   const Table = TableHOC<DataType>(
     columns,
@@ -101,7 +90,7 @@ const Customers = () => {
   return (
     <div className="admin-container">
       <AdminSidebar />
-      <main>{Table}</main>
+      <main>{isLoading ? <Skeleton length={10} /> : Table}</main>
     </div>
   );
 };
--- Frontend/src/redux/api/userAPI.ts
+++ Frontend/src/redux/api/userAPI.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-catch */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { User } from "../../types/types"
-import { MessageResponse, UserResponse } from "../../types/api-types";
+import { AllUsersResponse, DeleteUserRequest, MessageResponse, UserResponse } from "../../types/api-types";
 import axios from "axios";
 
 
@@ -12,13 +12,26 @@ export const userAPI =  createApi({
     reducerPath: 'userApi',
     // /api/v1/user/new
     baseQuery: fetchBaseQuery({baseUrl: `${import.meta.env.VITE_SERVER}/api/v1/user/`}),
+    tagTypes: ["users"],
     endpoints: (builder) => ({
         login: builder.mutation<MessageResponse, User>({
             query: (user) => ({
                 url: 'new',
                 method: "POST",
                 body: user
-            })
+            }),
+            invalidatesTags: ["users"],
+        }),
+        deleteUser: builder.mutation<MessageResponse, DeleteUserRequest>({
+            query: ({userId, adminUserId}) => ({
+                url: `${userId}?id=${adminUserId}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["users"],
+        }),
+        allUsers: builder.query<AllUsersResponse, string>({
+            query: (id) => `all?id=${id}`,
+            providesTags: ["users"],
         })
     })
 
@@ -34,4 +47,4 @@ export const getUser =async (id:string) => {
     }
 }
 
-export const {useLoginMutation} = userAPI;  // useLoginMutation is a hook that can be used in components to make a login request
+export const {useLoginMutation, useAllUsersQuery, useDeleteUserMutation} = userAPI;  // useLoginMutation is a hook that can be used in components to make a login request
